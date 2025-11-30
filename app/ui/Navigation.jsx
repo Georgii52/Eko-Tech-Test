491220c0f2c6aa5b49cee9f1a53bb7f1e5f55951
Georgii Momdzhi <momdzhiphoto@gmail.com>
2025-11-30 23:14:19 +0300

    fixes to ui, added one more dropdown menu

--- app/ui/Navigation.jsx
+++ app/ui/Navigation.jsx
@@ -6,13 +6,13 @@ import { useState, useEffect } from "react"
 export default function Navigation () {
     const [isOpen, setIsOpen] = useState (false)
     const [isDisabled, setIsDisabled] = useState (false)
-    const breakpoint = 1266
+    const maxBreakpoint = 1266
     const handleClick = () => {
         setIsOpen (prev => !prev)
     }
     useEffect (() => {
         const check = () => {
-            const inactive = (window.innerWidth >= breakpoint)
+            const inactive = (window.innerWidth >= maxBreakpoint)
             setIsDisabled(inactive)
             if (inactive && isOpen) {
                 setIsOpen(false)
@@ -21,7 +21,7 @@ export default function Navigation () {
         check ()
         window.addEventListener('resize', check)
         return () => window.removeEventListener('resize', check)
-    }, [isOpen, breakpoint])
+    }, [isOpen, maxBreakpoint])
     return (
         <div className="flex flex-row pt-[16px] px-[20px] md:px-[50px] items-center justify-start w-full font-medium text-black">
             <Image src={Logo} width={140} alt="Company logo" className=""/>
@@ -36,7 +36,7 @@ export default function Navigation () {
                 </button>
                 <DropDownMenu isOpen={isOpen} />
             </div>
-            <div className="hidden xl:flex flex-row flex-1 items-center justify-start gap-5">
+            <div className="hidden xl:flex flex-row flex-1 items-center justify-start gap-4">
                 <button className="flex flex-row items-center gap-[3px] text-[16px]">
                     Проекты
                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -88,12 +88,13 @@ export default function Navigation () {
                             <path d="M12.0833 2.25C14.725 2.25 16.5 4.76438 16.5 7.11C16.5 11.8603 9.13333 15.75 9 15.75C8.86667 15.75 1.5 11.8603 1.5 7.11C1.5 4.76438 3.275 2.25 5.91667 2.25C7.43333 2.25 8.425 3.01781 9 3.69281C9.575 3.01781 10.5667 2.25 12.0833 2.25Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                         </svg>
                     </button>
-                    <button className="bg-[#EAE3E199]/60 p-3 rounded-full hover:bg-[#EAE3E199] transition-all ease md:hidden">
+                    <button onClick={()=>handleClick()} disabled={isDisabled} className="bg-[#EAE3E199]/60 p-3 rounded-full hover:bg-[#EAE3E199] transition-all ease md:hidden relative">
+                    <DropDownMenuSmall isOpen={isOpen} />
                         <svg width="14" height="11" viewBox="0 0 14 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <line y1="0.5" x2="14" y2="0.5" stroke="black"/>
-                        <line y1="5.5" x2="14" y2="5.5" stroke="black"/>
-                        <line y1="10.5" x2="14" y2="10.5" stroke="black"/>
-                    </svg>
+                            <line y1="0.5" x2="14" y2="0.5" stroke="black"/>
+                            <line y1="5.5" x2="14" y2="5.5" stroke="black"/>
+                            <line y1="10.5" x2="14" y2="10.5" stroke="black"/>
+                        </svg>
                     </button>
                 </div>
             </div>
@@ -111,6 +112,59 @@ export function DropDownMenu ({ isOpen }) {
         ${isOpen ? 'opacity-100 scale-100 visible' : 'opacity-0 scale-95 invisible'}
         `}> 
             <a href="./">
+                <p className="flex flex-row items-center justify-start gap-2">
+                    Проекты
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="rotate-270">
+                        <path d="M3.5 5.25L7 8.75L10.5 5.25" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </p>
+            </a>
+            <a href="./">
+                <p className="flex flex-row items-center justify-start gap-2">
+                    Фото и видео
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="rotate-270">
+                        <path d="M3.5 5.25L7 8.75L10.5 5.25" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </p>
+            </a>
+            <a href="./">
+                <p className="flex flex-row items-center justify-start gap-2">
+                    Для клиентов
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="rotate-270">
+                        <path d="M3.5 5.25L7 8.75L10.5 5.25" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </p>
+            </a>
+            <a href="./">
+                <p className="flex flex-row items-center justify-start gap-2">
+                    О компании
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="rotate-270">
+                        <path d="M3.5 5.25L7 8.75L10.5 5.25" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </p>
+            </a>
+            <a href="./">
+                <p className="flex flex-row items-center justify-start gap-2">
+                    Услуги
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="rotate-270">
+                        <path d="M3.5 5.25L7 8.75L10.5 5.25" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    </svg>
+                </p>
+            </a>
+        </div>
+    )
+}
+
+export function DropDownMenuSmall ({ isOpen }) {
+    return (
+        <div className={`
+        absolute right-0 top-10 z-10 bg-gray-100 w-[450%] rounded-2xl
+        flex flex-col gap-2 px-5 py-2 items-start justify-between
+        transition-all duration-300 ease-out
+        origin-top-right
+        ${isOpen ? 'opacity-100 scale-100 visible' : 'opacity-0 scale-95 invisible'}
+        `}> 
+            <a href="./" className="hover:">
                 <p className="flex flex-row items-center justify-start gap-2">
                     Проекты
                     <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" className="rotate-270">
